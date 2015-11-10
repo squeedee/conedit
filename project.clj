e690cd3258f2d4ad1cc9f62182d87a7bbcc7a4d1
Rasheed Abdul-Aziz <squeedee@gmail.com>
2015-11-10 13:36:42 -0500

    Fixed source paths in leiningen

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject conedit "0.1.0-SNAPSHOT"
   :description "Concourse Pipeline Editor"
+  :source-paths ["src/clj" "src/cljs"]
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.145"]
                  [org.omcljs/om "1.0.0-alpha14"]
