4a0249b0aad0da7f2b39134763a1b9df36d14c92
Rasheed Abdul-Aziz <squeedee@gmail.com>
2015-11-10 10:35:51 -0500

    We got started

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject conedit "0.1.0-SNAPSHOT"
+  :description "Concourse Pipeline Editor"
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.145"]
+                 [org.omcljs/om "1.0.0-alpha14"]
+                 [figwheel-sidecar "0.4.0" :scope "provided"]])
